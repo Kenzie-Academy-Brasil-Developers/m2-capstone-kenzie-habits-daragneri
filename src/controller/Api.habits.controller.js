07d0fb69ef82f71bf26bed2e9d4f3d855c7fee8a
danmatuoka <dan_mtk@hotmail.com>
2022-07-12 16:08:38 -0300

    feat: verificacao login && fix: requisicoes

--- src/controller/Api.habits.controller.js
+++ src/controller/Api.habits.controller.js
@@ -1,85 +1,85 @@
-export default class Habits{
+export default class Habits {
     static baseUrl = 'https://habits-kenzie.herokuapp.com'
 
-    static async createHabit(data){
+    static async createHabit(data) {
         const token = JSON.parse(localStorage.getItem('@captsone:token'))
         return await fetch(`${this.baseUrl}/api/habits`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 
-    static async readAllHabits(){
-        const token = JSON.parse(localStorage.getItem('@captsone:token'))
+    static async readAllHabits() {
+        const token = localStorage.getItem('@captsone:token')
         return await fetch(`${this.baseUrl}/api/habits`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
-    
-    static async getHabitByCategory(category){
-        const token = JSON.parse(localStorage.getItem('@captsone:token'))
+
+    static async getHabitByCategory(category) {
+        const token = localStorage.getItem('@captsone:token')
         return await fetch(`${this.baseUrl}/api/habits/category/:${category}`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 
-    static async updateHabit(data, habitId){
-        const token = JSON.parse(localStorage.getItem('@captsone:token'))
+    static async updateHabit(data, habitId) {
+        const token = localStorage.getItem('@captsone:token')
         return await fetch(`${this.baseUrl}/api/habits/:${habitId}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 
-    static async completeHabit(habitId){
-        const token = JSON.parse(localStorage.getItem('@captsone:token'))
-        return await fetch(`${this.baseUrl}/api/habits/complete/:${habitId}`, {
-            method: "PATCH",
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+    static async completeHabit(habitId) {
+        const token = localStorage.getItem('@captsone:token')
+        return await fetch(`${this.baseUrl}/api/habits/complete/${habitId}`, {
+                method: "PATCH",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 
-    static async deleteHabit(habitId){
-        const token = JSON.parse(localStorage.getItem('@captsone:token'))
-        return await fetch(`${this.baseUrl}/api/habits/:${habitId}`, {
-            method: "DELETE",
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err))
+    static async deleteHabit(habitId) {
+        const token = localStorage.getItem('@captsone:token')
+        return await fetch(`${this.baseUrl}/api/habits/${habitId}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 }
